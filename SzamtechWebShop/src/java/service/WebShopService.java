ce92834d9e888a88214317a8679fbc0dc8d73de3
Klepe Dominik <domaklepe@gmail.com>
2021-11-02 17:15:32 +0100

    Jelszó titkosítás
    Ecypt/decrypt függvények serviceréteghez való hozzáadása

--- SzamtechWebShop/src/java/service/WebShopService.java
+++ SzamtechWebShop/src/java/service/WebShopService.java
@@ -1,5 +1,69 @@
 package service;
 
+import java.security.spec.KeySpec;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESedeKeySpec;
+import org.apache.commons.codec.binary.Base64;
+
 public class WebShopService {
     
+    
+    
+    // <editor-fold defaultstate="collapsed" desc="Encryption/Decryption">
+    /**
+     * 
+     * Az ures WebShopService felelős a jelszótitkosítás elindításához
+     * Ez azért lehetséges mert másképp egyedet nem képzek belőle majd
+     * encrypt funkcio felelos a jelszo kodolasaert
+     * decrypt funkcio felelos a jelszo dekodolasaert
+     * 
+     */
+    private static final String UNICODE_FORMAT = "UTF8";
+    public static final String DESEDE_ENCRYPTION_SCHEME = "DESede";
+    private KeySpec ks;
+    private SecretKeyFactory skf;
+    private Cipher cipher;
+    byte[] arrayBytes;
+    private String myEncryptionKey;
+    private String myEncryptionScheme;
+    SecretKey key;
+    //kodolas elkezdesehez obj letrehozasa
+    public WebShopService() throws Exception {
+        myEncryptionKey = "ThisIsSpartaThisIsSparta";
+        myEncryptionScheme = DESEDE_ENCRYPTION_SCHEME;
+        arrayBytes = myEncryptionKey.getBytes(UNICODE_FORMAT);
+        ks = new DESedeKeySpec(arrayBytes);
+        skf = SecretKeyFactory.getInstance(myEncryptionScheme);
+        cipher = Cipher.getInstance(myEncryptionScheme);
+        key = skf.generateSecret(ks);
+    }
+    //Kodolas
+    public String encrypt(String unencryptedString) {
+        String encryptedString = null;
+        try {
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] plainText = unencryptedString.getBytes(UNICODE_FORMAT);
+            byte[] encryptedText = cipher.doFinal(plainText);
+            encryptedString = new String(Base64.encodeBase64(encryptedText));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return encryptedString;
+    }
+    //Dekocolas
+    public String decrypt(String encryptedString) {
+        String decryptedText=null;
+        try {
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] encryptedText = Base64.decodeBase64(encryptedString);
+            byte[] plainText = cipher.doFinal(encryptedText);
+            decryptedText= new String(plainText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return decryptedText;
+    }
+    // </editor-fold>
 }
